8cc0e5b3c5516cff6d9e9d0b24012c543ee9bc58
Jarmo Juujärvi <jaanjuuj@sovproj09.it.jyu.fi>
2016-02-24 10:07:31 +0200

    ---

--- src/main/java/com/moveatis/lotas/managedbeans/ObservationManagedBean.java
+++ src/main/java/com/moveatis/lotas/managedbeans/ObservationManagedBean.java
@@ -3,6 +3,9 @@ package com.moveatis.lotas.managedbeans;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -11,11 +14,39 @@ import java.io.Serializable;
 @Named(value = "observationBean")
 @SessionScoped
 public class ObservationManagedBean implements Serializable {
+    
+    private List<String> types;
+
+    public List<String> getTypes() {
+        return types;
+    }
 
     /**
      * Creates a new instance of ObservationManagedBean
      */
     public ObservationManagedBean() {
+        types = new ArrayList<>();
+        types.add("Järjestelyä");
+        types.add("Ohjeiden anto");
+        types.add("Tehtävien suoritus");
+        types.add("Palautteen anto");
+        types.add("Lopettelu");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add("Dummy dataa");
+        types.add(FacesContext.class.getPackage().getImplementationVersion());
     }
     
 }
