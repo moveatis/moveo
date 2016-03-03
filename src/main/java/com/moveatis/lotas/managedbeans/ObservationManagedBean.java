a2c3781b357ece5c146d3ca4bd7febfaebeb1100
Ilari Paananen <ilari.k.paananen@student.jyu.fi>
2016-03-03 14:58:18 +0200

    Observer: Moved recording adding to observation bean.

--- src/main/java/com/moveatis/lotas/managedbeans/ObservationManagedBean.java
+++ src/main/java/com/moveatis/lotas/managedbeans/ObservationManagedBean.java
@@ -3,6 +3,8 @@ package com.moveatis.lotas.managedbeans;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.Map;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -18,4 +20,20 @@ public class ObservationManagedBean implements Serializable {
     public ObservationManagedBean() {
     }
     
+    //
+    // NOTE: Is this the right managed bean for adding recordings?
+    //
+    
+    private String recording = "no recording";
+    
+    public void addRecording() {
+        Map<String, String> requestParameters = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String startTime = requestParameters.get("start-time");
+        String endTime = requestParameters.get("end-time");
+        recording = startTime + " --> " + endTime;
+    }
+    
+    public String getRecording() {
+        return recording;
+    }
 }
