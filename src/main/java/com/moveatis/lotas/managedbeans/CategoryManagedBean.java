3b7cb56bd7b37062bd289dd5d08150fb8b06d4e7
Ilari Paananen <ilari.k.paananen@student.jyu.fi>
2016-03-21 17:19:07 +0200

    Initial category selection view.

--- src/main/java/com/moveatis/lotas/managedbeans/CategoryManagedBean.java
+++ src/main/java/com/moveatis/lotas/managedbeans/CategoryManagedBean.java
@@ -3,6 +3,9 @@ package com.moveatis.lotas.managedbeans;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
 
 /**
  *
@@ -20,4 +23,91 @@ public class CategoryManagedBean implements Serializable {
     public CategoryManagedBean() {
     }
     
+    //
+    //
+    //
+    
+    public class CategorySet {
+        private String id;
+        private String name;
+        private List<String> categories;
+        private String[] selectedCategories;
+        
+        public CategorySet(String id) {
+            this.id = id;
+        }
+        
+        public String getId() {
+            return id;
+        }
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public List<String> getCategories() {
+            return categories;
+        }
+        
+        public void setCategories(List<String> categories) {
+            this.categories = categories;
+        }
+        
+        public String[] getSelectedCategories() {
+            return selectedCategories;
+        }
+        
+        public void setSelectedCategories(String[] selectedCategories) {
+            this.selectedCategories = selectedCategories;
+        }
+    }
+    
+    private List<CategorySet> categorySets;
+    
+    private List<String> categories;
+    private String[] selectedCategories;
+    
+    @PostConstruct
+    public void init() {
+        categories = new ArrayList<>();
+        categories.add("Järjestelyt");
+        categories.add("Tehtävän selitys");
+        categories.add("Ohjaus");
+        categories.add("Palautteen anto");
+        categories.add("Tarkkailu");
+        categories.add("Muu toiminta");
+
+        List<String> oppilaanToiminnot = new ArrayList<>();
+        oppilaanToiminnot.add("Oppilas suorittaa tehtävää");
+        
+        categorySets = new ArrayList<>();
+        CategorySet categorySet = new CategorySet("opettajan-toiminnot");
+        categorySet.setName("Opettajan toiminnot");
+        categorySet.setCategories(categories);
+        categorySets.add(categorySet);
+        categorySet = new CategorySet("oppilaan-toiminnot");
+        categorySet.setName("Oppilaan toiminnot");
+        categorySet.setCategories(oppilaanToiminnot);
+        categorySets.add(categorySet);
+    }
+    
+    public List<String> getCategories() {
+        return categories;
+    }
+    
+    public String[] getSelectedCategories() {
+        return selectedCategories;
+    }
+    
+    public void setSelectedCategories(String[] selectedCategories) {
+        this.selectedCategories = selectedCategories;
+    }
+    
+    public List<CategorySet> getCategorySets() {
+        return categorySets;
+    }
 }
