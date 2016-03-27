8cbda78f01451c21dc2b886200d88d429fd1702a
Sami Kallio <phinaliumz@outlook.com>
2016-03-27 21:44:16 +0300

    First step toward database connection

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
 polku=`pwd`/docker
+tietokannanPolku=`pwd`/db
 
-docker run --privileged=true -it -p 8080:8080 -p 8787:8787  -v $polku:/opt/wildfly/standalone/deployments moveatis/moveatis-devel
+# Käynnistetään Postgresql:n sisältämä docker-image
+docker run -d -p 5432:5432 --name moveatis-db -v $tietokannanPolku:/var/lib/postgresql/data \ 
+-e 'DB_USER=moveatis' \ 
+-e 'DB_PASS=lotas' \ 
+-e 'DB_NAME=moveatisdb'\
+centos/postgresql
+
+# Käynnistetään Wildfly:n sisältämä docker-image
+docker run --privileged=true -it -p 8080:8080 -p 8787:8787 --name moveatis-wildfly --link moveatis-db:lotasdb -v $polku:/opt/wildfly/standalone/deployments moveatis/moveatis-devel
