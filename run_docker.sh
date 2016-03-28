40f9f916e5605bb613cd3d9ed1185208639d8747
Sami Kallio <phinaliumz@outlook.com>
2016-03-28 15:46:23 +0300

    Added Wildfly-admin port.

--- run_docker.sh
+++ run_docker.sh
@@ -7,4 +7,4 @@ tietokannanPolku=`pwd`/db
 docker run -d -p 5432:5432 --name moveatis-db -v $tietokannanPolku:/var/lib/postgresql/data -e 'DB_USER=moveatis' -e 'DB_PASS=lotas' -e 'DB_NAME=moveatisdb' centos/postgresql
 
 # Käynnistetään Wildfly:n sisältämä docker-image
-docker run --privileged=true -it -p 8080:8080 -p 8787:8787 --name moveatis-wildfly --link moveatis-db:lotasdb -v $polku:/opt/wildfly/standalone/deployments moveatis/moveatis-devel
+docker run --privileged=true -it -p 8080:8080 -p 8787:8787 -p 9990:9990 --name moveatis-wildfly --link moveatis-db:lotasdb -v $polku:/opt/wildfly/standalone/deployments moveatis/moveatis-devel
