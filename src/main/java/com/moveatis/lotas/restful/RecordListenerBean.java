236bcc8bb0f62a5cc460fc8779efd91a3a543b06
Sami Kallio <phinaliumz@outlook.com>
2016-04-03 19:15:47 +0300

    Lisenssitekstin lisäys muutamiin luokkiin.

--- src/main/java/com/moveatis/lotas/restful/RecordListenerBean.java
+++ src/main/java/com/moveatis/lotas/restful/RecordListenerBean.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright 2016 Sami Kallio.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the University of Jyväskylä nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.moveatis.lotas.restful;
 
 import com.moveatis.lotas.enums.UserType;
@@ -7,10 +35,8 @@ import com.moveatis.lotas.records.RecordEntity;
 import com.moveatis.lotas.session.SessionBean;
 import java.io.Serializable;
 import java.io.StringReader;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
-import javax.faces.bean.ManagedProperty;
 import javax.inject.Named;
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -85,14 +111,6 @@ public class RecordListenerBean implements Serializable {
         
         for (int i = 0; i < array.size(); i++) {
             JsonObject object = array.getJsonObject(i);
-//        observationBean.setStartTime(obs.getString("startTime"));
-//        observationBean.setEndTime(obs.getLong("endTime"));
-//        
-//        JSONArray categoryArray = obs.getJSONArray("categories");
-//        for (int i = 0; i < array.length(); i++) {
-//            observationBean.addCategory(categoryArray.getString(i));
-//        }
-            
             RecordEntity record = new RecordEntity();
             
             if(categoryEJB.find(object.getString("category")) != null) {
