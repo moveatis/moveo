361879bd897622af4db8b55554426f3bdf26faa6
jumois <juha.pa.moisio@student.jyu.fi>
2016-04-06 16:52:03 +0300

    Add BSD-3 License header to every source file.
    I used Lisence Changer plugin for NetBeans
    http://www.codejava.net/ides/netbeans/how-to-update-license-header-for-java-files-in-netbeans
    
    License template file for NetBeans is in bsd3-license-template.txt file.
    
    Added also LICENSE file in the project root.

--- src/main/java/com/moveatis/lotas/managedbeans/SummaryManagedBean.java
+++ src/main/java/com/moveatis/lotas/managedbeans/SummaryManagedBean.java
@@ -1,3 +1,32 @@
+/* 
+ * Copyright (c) 2016, Jarmo Juujärvi, Sami Kallio, Kai Korhonen, Juha Moisio, Ilari Paananen 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     3. Neither the name of the copyright holder nor the names of its 
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.moveatis.lotas.managedbeans;
 
 import com.moveatis.lotas.interfaces.Observation;
@@ -45,6 +74,9 @@ public class SummaryManagedBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SummaryManagedBean.class);
 
+    /**
+     *
+     */
     public SummaryManagedBean() {
         this.locale = new Locale("fi", "FI"); // get users locale from session bean ?
         this.timeZone = TimeZone.getTimeZone("UTC"); // this should be the servers timezone
@@ -55,59 +87,114 @@ public class SummaryManagedBean {
         this.zoomMax = 24 * 60 * 60 * 1000;
     }
 
+    /**
+     *
+     */
     @PostConstruct
     protected void initialize() {
         createTimeline();
     }
 
+    /**
+     *
+     * @return
+     */
     public TimelineModel getTimeline() {
         return timeline;
     }  
 
+    /**
+     *
+     * @return
+     */
     public Locale getLocale() {
         return locale;  
     }  
 
+    /**
+     *
+     * @param locale
+     */
     public void setLocale(Locale locale) {
         this.locale = locale;  
     }
 
+    /**
+     *
+     * @return
+     */
     public TimeZone getTimeZone() {
         return timeZone;
     }
 
+    /**
+     *
+     * @param timeZone
+     */
     public void setTimeZone(TimeZone timeZone) {
         this.timeZone = timeZone;
     }
 
+    /**
+     *
+     * @return
+     */
     public TimeZone getBrowserTimeZone() {
         return browserTimeZone;
     }
 
+    /**
+     *
+     * @param browserTimeZone
+     */
     public void setBrowserTimeZone(TimeZone browserTimeZone) {
         this.browserTimeZone = browserTimeZone;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getMin() {
         return min;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getMax() {
         return max;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getStart() {
         return start;
     }
 
+    /**
+     *
+     * @return
+     */
     public long getZoomMin() {
         return zoomMin;
     }
 
+    /**
+     *
+     * @return
+     */
     public long getZoomMax() {
         return zoomMax;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getObservationName() {
         //return observationBean.getName();
         Date date = new Date();
@@ -115,21 +202,37 @@ public class SummaryManagedBean {
         return "Observointi - " + df.format(date);
     }
 
+    /**
+     *
+     * @return
+     */
     public String getObservationTarget() {
         //return observationBean.getTarget();
         return "";
     }
 
+    /**
+     *
+     * @return
+     */
     public String getObservationDescription() {
         //return observationBean.getDescription();
         return "";
     }
 
+    /**
+     *
+     * @return
+     */
     public String getObservationDuration() {
         // return observationBean.getDuration();
         return "??h ??m ??s";
     }
 
+    /**
+     *
+     * @return
+     */
     public long getEndTime() {
         return observationBean.getEndTime();
     }
